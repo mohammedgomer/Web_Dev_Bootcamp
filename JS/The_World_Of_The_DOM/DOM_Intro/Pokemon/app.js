6b92fc3d7534aca1f851efbc2bd5f07272586a35
mohammedgomer <mg.omer@outlook.com>
2021-10-01 19:56:23 +0100

    Final DOM Intro content

--- JS/The_World_Of_The_DOM/DOM_Intro/Pokemon/app.js
+++ JS/The_World_Of_The_DOM/DOM_Intro/Pokemon/app.js
@@ -1,19 +1,19 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
 
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i < 899; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
     label.innerText = `#${i}`;
     const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
+    newImg.src = `${baseUrl}${i}.png`
 
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    container.appendChild(pokemon);
+
+
 }
